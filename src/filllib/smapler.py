3b2fdc5b62e2ca1602e66e0a5d631452fa1190ff
Fanis Samaras <NisFa>
2026-05-30 23:33:34 +0300

    Ultrasonic

---
+++ src/filllib/smapler.py
@@ -0,0 +1,9 @@
+from gpiozero import DistanceSensor
+from time import sleep
+
+sensor = DistanceSensor(echo=24,trigger=23)
+
+while True:
+    print(f"Distance:{sensor.distance*100} cm")
+    sleep(1)
+
