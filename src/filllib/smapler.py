186902751c5d0ac5f608aba5398618c4a8a4a1fa
Fanis Samaras <NisFa>
2026-05-30 23:54:24 +0300

    Test

--- src/filllib/smapler.py
+++ src/filllib/smapler.py
@@ -1,12 +1,13 @@
 from gpiozero import DistanceSensor
 from time import sleep
 
-lista  = []
+lista = []
 total_sum = 0 
 sensor = DistanceSensor(echo=24,trigger=23)
 
-for i in range(1000):
+for i in range(16):
     lista.append(sensor.distance)
+    sleep(1)
 
 for item in lista:
     total_sum += item
